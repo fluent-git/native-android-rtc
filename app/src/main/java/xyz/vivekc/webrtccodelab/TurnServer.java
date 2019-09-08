aad26cb63597f0e456aee13ae2aebfda3faedd04
Yoel <yoelsusantopro@yahoo.com>
2019-09-08 14:41:00 +0700

    Initial commit

---
+++ app/src/main/java/xyz/vivekc/webrtccodelab/TurnServer.java
@@ -0,0 +1,17 @@
+package xyz.vivekc.webrtccodelab;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.PUT;
+
+/**
+ * Webrtc_Step3
+ * Created by vivek-3102 on 11/03/17.
+ */
+
+
+public interface TurnServer {
+    @PUT("/_turn/MyFirstApp")
+    Call<TurnServerPojo> getIceCandidates(@Header("Authorization") String authkey);
+}
